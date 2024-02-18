7e675b9e90de1449bf6d6f64f01f4229a2d284cb
JD Fillmore <jd@infused.agency>
2024-02-18 11:08:32 -0500

    WIP - home page changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import FAQ from "../../components/Home/FAQ/faq";
 import About from "../../components/Home/About/about";
 import CTA from "../../components/CTA/cta";
 import Footer from "../../components/Footer/footer";
+import Button from "../../components/Button/button";
 
 import { Helmet } from "react-helmet";
 import Link from "next/link";
@@ -128,7 +129,7 @@ const Home = () => {
       <Nav />
       <Hero
         title="Niagara Web Design Company"
-        titleTwo="We build websites that help grow your business."
+        titleTwo="We build lead-generating websites for Niagara businesses."
         subTitle="Infused Agency is a top Niagara web design agency hand-crafting beautiful website design since 2013."
       />
       <ClientLogos
@@ -287,13 +288,13 @@ const Home = () => {
                 </h2>
               </div>
             </div>
-            <div className="row text-center pd-top-40">
+            <div className="row text-center pd-top-40 pd-btm-120">
               <div className="col-lg-4">
                 <Image className="img-fluid" src={viscaTestimonial} alt="visca electric - web design niagara testimonial"/>
                 <h3>Sandro Visca</h3>
                 <p>Owner - VISCA electric</p>
                 <p className="testimonial">
-                  "The design and development process was smooth and easy."
+                  "The design and development process was smooth & easy."
                 </p>
                 <p className="full-testimonial">
                 Working with Infused Agency was a pleasure. He was extremely responsive and informative from start to finish. Infused Agency is creative, efficient, and very knowledgeable in their field. They helped me and my team create a new website for my business: VISCA electric. Check it out! viscaelectric.ca. The design and development process was smooth and easy. I would absolutely recommend Infused Agency to anyone looking for a new or updated website for their business!
@@ -325,14 +326,48 @@ const Home = () => {
           </div>
         </section>
       </Fade>
+      <Fade>
+      <section className="cta home-cta">
+        <div className="container">
+          <div className="row text-center">
+            <div className="col-lg-12">
+              <h2>Ready to <span>Grow Your Business</span> With
+Niagara Web Design Services?</h2>
+<p>Click the button below for a free quote</p>
+              <a
+                href="https://calendly.com/infused-agency/project-discovery/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button>
+                  Get Free Quote
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 512 512"
+                    class="absolute left-1/2 top-1/2 translate-centered group-hover:ml-1 transition-250"
+                    height="20px"
+                    width="20px"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M295.6 163.7c-5.1 5-5.1 13.3-.1 18.4l60.8 60.9H124.9c-7.1 0-12.9 5.8-12.9 13s5.8 13 12.9 13h231.3l-60.8 60.9c-5 5.1-4.9 13.3.1 18.4 5.1 5 13.2 5 18.3-.1l82.4-83c1.1-1.2 2-2.5 2.7-4.1.7-1.6 1-3.3 1-5 0-3.4-1.3-6.6-3.7-9.1l-82.4-83c-4.9-5.2-13.1-5.3-18.2-.3z"></path>
+                  </svg>
+                </Button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      </Fade>
+      <Fade>
       <section className="home-services">
         <div className="container">
           <div className="row pd-btm-80">
             <div className="col-lg-12 text-center">
-              <h2>Our Web Design &amp; Marketing Services</h2>
+              <h2>Niagara Web Design &amp; SEO Services <span>That Work</span></h2>
               <p className="serv-intro">
-                We design <Link href="/web-design">mobile-friendly websites</Link> that help grow your business. From
-                our initial discovery meeting to post-launch support, we're always here to help.
+                We Provide Custom Web Design &amp; SEO Solutions For Your Unique Business Needs
               </p>
             </div>
           </div>
@@ -343,132 +378,40 @@ const Home = () => {
             <div className="col-lg-4">
               <div className="card">
                 <Image className="img-fluid" src={webImg} alt="web designer niagara" />
-                <h2>Web Design &amp; Development</h2>
-                <p>We create websites to help attract more customers.</p>
-                <Link href="/web-design">Hand-Coded Websites</Link>
+                <Link href="/web-design"><h2>Web Design &amp; Development</h2></Link>
+                <p>Your business and online image are reflected in your website. At Infused Agency, our Niagara web design services elevate brands by creating impactful websites that excel in turning visitors into customers. Our skilled web designers and website developers deliver top-notch mockups and websites, ensuring that your site aligns perfectly with your brand. With our versatile web development team, we embrace the latest design trends to provide your customers with a smooth browsing experience.</p>
               </div>
             </div>
             <div className="col-lg-4">
               <div className="card">
                 <Image className="img-fluid" src={wpImg} alt="niagara web design company" />
-                <h2>WordPress Development</h2>
-                <p>Update your website without the need of a developer.</p>
-                <Link href="/wordpress-developer-niagara/">WordPress Websites</Link>
+                <Link href="/wordpress-developer-niagara"><h2>WordPress Development</h2></Link>
+                <p>Your business and online identity shine through your website, and at Infused Agency, we leverage the unparalleled benefits of WordPress to elevate your brand. Our Niagara web design services specialize in harnessing the power of the content management system WordPress, creating impactful websites that not only reflect your brand essence but also offer exceptional advantages. With WordPress, you'll have the ability to update your website without our help. Our skilled team of web designers and developers ensures that your WordPress site is not just visually appealing but also fully optimized for performance.</p>
               </div>
             </div>
             <div className="col-lg-4">
               <div className="card">
                 <Image className="img-fluid" src={seoImg} alt="best web designer niagara" />
-                <h2>SEO</h2>
-                <p>We help rank your website on the first page of Google.</p>
-                <Link href="/seo-niagara/">SEO Services</Link>
+                <Link href="/niagara-seo-company"><h2>SEO</h2></Link>
+                <p>The #1 goal of SEO is to place your website at the top of the search engine results page (SERP). Infused Agency's Niagara web design company is skilled in search engine optimization well-versed in creating SEO strategies. We understand what it takes to move the needle, placing your website to the top of the search results for relevant keyword. We start with a full SEO audit, extensive custom keyword research and implement proper SEO strategies from there.</p>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <section className="home-feat">
+      </Fade>
+      <Fade>
+      <section className="why-infused">
         <div className="container">
-          <div className="row">
+          <div className="row text-center">
             <div className="col-lg-12">
-              <div className="text-center pd-top-120 pd-btm-80">
-                <Fade>
-                  <h2>Featured Project</h2>
-                  <p id="sub">
-                    Take a look and see the huge difference we've made to our client's online existences, with our
-                    Niagara web design services.
-                  </p>
-                  <br />
-                  <Image className="img-fluid" src={featureImg} alt="web design niagara" />
-                  <br />
-                  <p className="text-center">
-                    <strong>
-                      Latest Launch -{" "}
-                      <a target="_blank" href="https://viscaelectric.ca">
-                        Visca Electric
-                      </a>
-                    </strong>
-                  </p>
-                  <em>
-                    "Working with Infused Agency was a pleasure. They were extremely responsive and informative from
-                    start to finish. They are creative, efficient, and very knowledgeable in their field."
-                  </em>
-                  <Link className="d-block text-center pd-top-40" href="/work">
-                    <button>View More Work</button>
-                  </Link>
-                </Fade>
-              </div>
+              <h2>Why <span>Choose Us</span> As Your Niagara Web Design Company</h2>
+              <p>We Grow Businesses Through Lead-Generating Website Designs &amp; SEO Strategies</p>
             </div>
           </div>
         </div>
       </section>
-      <WhatWeDo
-        title="Web Design Services in Niagara"
-        subTitle={[
-          "As a small business ourselves, we understand that your small business cannot fork out upfront tens of thousands of dollars for a ",
-          <Link href="/services">website</Link>,
-          ". That's why we offer local and affordable ",
-          <Link href="/work">website design</Link>,
-          " solutions to fit your small business budget. Our websites are secure and modern, which helps bring in more local leads near you and revenue through your new website.",
-        ]}
-        boxOneTitle="Responsive"
-        boxOneDesc="Your website will respond and adapt to any device - phone, tablet, desktop computer, you name it."
-        boxOneImgAlt="web design"
-        boxTwoTitle="Optimized"
-        boxTwoDesc="Every website is hand-coded and optimized, which leads to much better security and website performance."
-        boxTwoImgAlt="Optimized"
-        boxThreeTitle="Secure"
-        boxThreeDesc="Our websites are secure. You can sleep well at night knowing it won't be compromised."
-        boxThreeImgAlt="Optimized"
-        whatTitle="What Makes A Good Website?"
-        whatText={[
-          "Does your current ",
-          <Link href="/services">website</Link>,
-          " not bring in new leads? Sometimes website problems are obvious like when a website is not mobile-friendly or if a website doesn't have a modern design. Other times, a closer inspection into your website's practices are required in order to identify issues. Most issues relating to any website are that it doesn't rank on the first page of Google, which means it's SEO isn't up to par. A website's message that doesn't align with your customers, or a website that doesn't convert users into paying customers.",
-        ]}
-        servicesTitle="Why Is Hiring a Good Web Development Company Important?"
-        servicesText={[
-          "Your website acts as your brand's central hub on the internet. The fact that your website is the first stop to any new customers is absolutely vital to your business, in terms of gaining new revenue from your business. This means that you need the best local web development company to deliver great website results to your business. Infused Agency makes sure that we do a custom-tailored website job right from the start, to ensure your website is a success and is optimized.",
-        ]}
-        whatKindTitle="What Type of Services Do You Need?"
-        whatKindText={[
-          "Whether you want to update the look of your website or need to manage your current website, we can do it all. We strive to create a quality website that helps bring in new revenue to your business. You will have a highly professional and skilled web developer who can answer any website-related questions you may have when it comes to your new website. For the best local web development services, call on our trusted professionals. We're here to assess your current website and provide our own website solutions to help your business grow.",
-        ]}
-      />
-
-      <Clients title="Our happy clients" />
-      <FAQ />
-      <About
-        city="Niagara"
-        intro={[
-          "The Regional Municipality of ",
-          <a href="https://www.niagararegion.ca/">Niagara</a>,
-          " in Southern Ontario, Canada, lies on the west side of the Niagara River, between lakes Ontario and Erie. The Canadian section of Niagara Falls, including the dramatic Horseshoe Falls, is its most famous feature.",
-        ]}
-        area="1,854"
-        population="447,888"
-        topSights={[
-          <a href="https://www.niagaraparks.com/visit-niagara-parks/plan-your-visit/niagara-falls-geology-facts-figures/">
-            Niagara Falls
-          </a>,
-          ", ",
-          <a href="https://www.niagaraparks.com/visit/attractions/journey-behind-the-falls/">
-            Journey Behind The Falls
-          </a>,
-          ", ",
-          <a href="https://www.skylon.com/">Skylon Tower</a>,
-        ]}
-        qOne="What is Niagara in Canada?"
-        qOneAns="Niagara Falls is a city in Ontario. It's near the west bank of the Niagara River in the Golden Horseshoe region of Southern Ontario."
-        qTwo="What is Niagara known for?"
-        qTwoAns="Niagara is known for having one of the most famous waterfalls in the entire world. It's been a popular tourist attraction for over 200 years, and it's also a major source of electric power for the surrounding region."
-        qThree="Is Niagara dangerous?"
-        qThreeAns="Even though Niagara has the falls and it's beautiful, Niagara itself actually is not a very safe place. 91% of cities are safer than Niagara, but 9% of cities are more dangerous than the region."
-        qFour="What is the best time of year to go to Niagara?"
-        qFourAns="The best time to visit Niagara FAlls is between the months of June and August. Summer is the best time to go because the mist coming off of Niagara falls can make the area feel much cooler."
-        mapURL="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d746182.1554938729!2d-79.90704906963097!3d43.06721961311654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d34c9ec993deb7%3A0xef7f46930d192396!2sRegional%20Municipality%20of%20Niagara%2C%20ON!5e0!3m2!1sen!2sca!4v1628642324816!5m2!1sen!2sca"
-      />
-      <CTA />
+      </Fade>
       <Footer />
     </>
   );
