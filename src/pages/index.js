c21028e7e2dce6e1f2b2fca784fdec6452960f08
J.D. Fillmore <jdfillmore.dev@gmail.com>
2024-06-26 21:42:33 -0400

    Add calendly cta to all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { InlineWidget } from "react-calendly";
 import {
   Accordion,
   AccordionItem,
@@ -747,8 +748,14 @@ const Home = () => {
               <h2>
                 Ready to <span>Grow Your Business</span>?
               </h2>
-              <p>Click the button below for a free quote</p>
-              <a
+              <p>Get your free quote below!</p>
+              <InlineWidget
+                styles={{
+                  height: "700px",
+                }}
+                url="https://calendly.com/infused-agency/project-discovery/"
+              />
+              {/* <a
                 href="https://calendly.com/infused-agency/project-discovery/"
                 target="_blank"
                 rel="noreferrer"
@@ -768,7 +775,7 @@ const Home = () => {
                     <path d="M295.6 163.7c-5.1 5-5.1 13.3-.1 18.4l60.8 60.9H124.9c-7.1 0-12.9 5.8-12.9 13s5.8 13 12.9 13h231.3l-60.8 60.9c-5 5.1-4.9 13.3.1 18.4 5.1 5 13.2 5 18.3-.1l82.4-83c1.1-1.2 2-2.5 2.7-4.1.7-1.6 1-3.3 1-5 0-3.4-1.3-6.6-3.7-9.1l-82.4-83c-4.9-5.2-13.1-5.3-18.2-.3z"></path>
                   </svg>
                 </Button>
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
