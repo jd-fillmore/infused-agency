f754deb0038446c6a4cfb7fd036b41cf64b486ad
J.D. Fillmore <jdfillmore.dev@gmail.com>
2024-08-14 09:35:17 -0400

    Test to trigger deployment

--- next.config.js
+++ next.config.js
@@ -211,15 +211,15 @@ const nextConfig = {
       },
     ];
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.resolve.fallback = {
+  //       fs: false,
+  //     };
+  //   }
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
