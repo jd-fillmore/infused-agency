5f228f8e0ef8a104afb420d84f34c159413e43ee
J.D. Fillmore <jdfillmore.dev@gmail.com>
2024-09-23 13:34:31 -0400

    Remove popup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -97,12 +97,7 @@ export default function App({ Component, pageProps }) {
       })(window, document, "clarity", "script", "o3vik3tnlf");
   `}
       </Script>
-      <Script
-        src="https://cdn.popupsmart.com/bundle.js"
-        data-id="893567"
-        async
-        defer
-      ></Script>
+
       <Component {...pageProps} />
     </>
   );
