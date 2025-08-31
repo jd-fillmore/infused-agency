6369d40c4b3cdafa7c90590b42ccb660ddec873e
JD <jd@infused.agency>
2025-08-31 10:30:04 -0400

    Update sitemap

--- src/pages/sitemap.js
+++ src/pages/sitemap.js
@@ -123,8 +123,36 @@ export default function Sitemap() {
       url: "/seo/for-therapists-st-johns",
     },
     {
+      name: "SEO for Solar Companies Arizona",
+      url: "/seo/for-solar-companies-arizona",
+    },
+    {
       name: "SEO for Solar Companies Phoenix",
       url: "/seo/for-solar-companies-phoenix",
+    },
+    {
+      name: "SEO for Solar Companies Tucson",
+      url: "/seo/for-solar-companies-tucson",
+    },
+    {
+      name: "SEO for Solar Companies Mesa",
+      url: "/seo/for-solar-companies-mesa",
+    },
+    {
+      name: "SEO for Solar Companies Gilbert",
+      url: "/seo/for-solar-companies-gilbert",
+    },
+    {
+      name: "SEO for Solar Companies Chandler",
+      url: "/seo/for-solar-companies-chandler",
+    },
+    {
+      name: "SEO for Solar Companies Glendale",
+      url: "/seo/for-solar-companies-glendale",
+    },
+    {
+      name: "SEO for Solar Companies Scottsdale",
+      url: "/seo/for-solar-companies-scottsdale",
     },
   ];
 
@@ -133,7 +161,6 @@ export default function Sitemap() {
       <h1>Site Map</h1>
 
       <section>
-        <h2>SEO for Therapists</h2>
         <ul>
           {therapyLocations.map((location, index) => (
             <li key={index}>
